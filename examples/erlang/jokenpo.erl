96e83297df5af999e7377f24f5a98dd74cbcd177
Adolfo Neto <adolfo.usp@gmail.com>
2019-11-26 06:59:42 -0300

    Create jokenpo.erl

---
+++ examples/erlang/jokenpo.erl
@@ -0,0 +1,23 @@
+-module(jokenpo).
+-export([play/2]).
+
+play(X,X)  -> 
+    case lists:member(X, [rock, paper, scissors]) of
+        true -> 
+            {tie, X};
+        false ->
+            {"Error: invalid options!", {X, X}}
+    end;
+play(rock,paper) -> {win, paper};
+play(paper,rock) -> {win, paper};
+play(rock,scissors) -> {win, rock};
+play(scissors,rock) -> {win, rock};
+play(scissors,paper) -> {win, scissors};
+play(paper,scissors) -> {win, scissors};
+play(X,Y) -> {"Error: invalid options!", {X, Y}}.
+
+% c("jokenpo.erl").
+% jokenpo:play(paper, scissors).
+% will return {win,scissors}
+
+
