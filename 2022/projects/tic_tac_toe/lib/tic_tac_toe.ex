502d7f552e53e70d3e4ac0cdbe6c88b890d75a30
Adolfo Neto <adolfo.usp@gmail.com>
2022-11-23 12:43:47 -0300

    Some small refactorings, including tests and typespecs

--- 2022/projects/tic_tac_toe/lib/tic_tac_toe.ex
+++ 2022/projects/tic_tac_toe/lib/tic_tac_toe.ex
@@ -5,7 +5,7 @@ defmodule TicTacToe do
 
   @empty "_"
 
-  @spec new_board :: [...]
+  @spec new_board :: list
   @doc """
   Creates an empty board.
 
@@ -19,7 +19,24 @@ defmodule TicTacToe do
     List.duplicate(@empty, 9)
   end
 
-  @spec play(list, 1 | 2, number, number) :: list
+  @doc """
+  Registers a play (consisting of player, line and column) on a board. 
+
+  ## Examples
+
+      iex> TicTacToe.play(["_", "_", "_", "_", "_", "_", "_", "_", "_"], 1, 1, 1)
+      ["X", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+      iex> TicTacToe.play(["_", "_", "_", "_", "_", "_", "_", "_", "_"], 2, 1, 1)
+      ["O", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+      iex> TicTacToe.play(["X", "_", "_", "_", "_", "_", "_", "_", "_"], 2, 2, 2)
+      ["X", "_", "_", "_", "O", "_", "_", "_", "_"]
+
+      iex> TicTacToe.play(["X", "_", "_", "_", "O", "_", "_", "_", "_"], 1, 3, 1)
+      ["X", "_", "_", "_", "O", "_", "X", "_", "_"]
+  """
+  @spec play(list, 1..2, 1..3, 1..3) :: list
   def play(board, player, line, column)
       when line >= 1 and line <= 3 and column >= 1 and column <= 3 do
     if Enum.at(board, position(line, column)) == @empty do
@@ -27,6 +44,19 @@ defmodule TicTacToe do
     end
   end
 
+  @doc """
+  Returns true when a position is empty. Otherwise, it returns false. 
+
+  ## Examples
+
+      iex> TicTacToe.allowed_play?(["_", "_", "_", "_", "_", "_", "_", "_", "_"], 1, 1)
+      true
+
+      iex> TicTacToe.allowed_play?(["_", "_", "_", "_", "_", "X", "_", "_", "_"], 2, 3)
+      false
+
+  """
+  @spec allowed_play?(list, 1..3, 1..3) :: boolean()
   def allowed_play?(board, line, column) do
     Enum.at(board, position(line, column)) == @empty
   end
@@ -35,13 +65,26 @@ defmodule TicTacToe do
     (line - 1) * 3 + (column - 1)
   end
 
-  @spec empty_symbol :: <<_::8>>
+  @spec empty_symbol :: String.t()
   def empty_symbol(), do: @empty
 
-  @spec player_symbol(1 | 2) :: <<_::8>>
+  @spec player_symbol(1..2) :: String.t()
   def player_symbol(1), do: "X"
   def player_symbol(2), do: "O"
 
+  @doc """
+  Returns true when the board is in a state where the game is finished.
+
+  ## Examples
+
+      iex> TicTacToe.finished?(["X", "_", "_", "X", "_", "_", "X", "_", "_"])
+      {"X", true}
+
+      iex> TicTacToe.finished?(["O", "_", "_", "_", "O", "_", "_", "_", "O"])
+      {"O", true}
+
+  """
+  @spec finished?(list) :: {String.t() | nil, boolean()}
   def finished?([s, _, _, s, _, _, s, _, _]) when s != @empty, do: {s, true}
   def finished?([_, s, _, _, s, _, _, s, _]) when s != @empty, do: {s, true}
   def finished?([_, _, s, _, _, s, _, _, s]) when s != @empty, do: {s, true}
