6e6d04dfde63f6b973ca829dd164bae5d3b3f79b
Adolfo Neto <adolfo.usp@gmail.com>
2023-10-26 18:26:18 -0300

    Small changes

---
+++ classes_tools/elixir/livebook/notebooks/10-24-2023/state.md
@@ -0,0 +1,71 @@
+- EXERCISM
+
+1 Alessandro Bueno
+VM499:2 2 Derli Machado
+VM499:2 3 Diogo Dias
+OK VM499:2 4 Gabriel Magalhaes
+VM499:2 5 Gabriel Shimosaka
+OK VM499:2 6 GERALDO SAMIR SILVEIRA VARRIALE
+VM499:2 7 Igor Vinícius
+(INTERNET NOT GOOD) VM499:2 8 LETICIA D SANTI MOREIRA
+VM499:2 9 Lucas Kemp de Oliveira
+VM499:2 10 LUCIANO ANTONICHEN DE CAMPOS
+?? VM499:2 11 Maike Cristian
+VM499:2 12 Michelangelo Cali
+?? VM499:2 13 RAUL SCARMOCIN DE FREITAS
+VM499:2 14 Ricardo Dos S. Alvares
+VM499:2 15 Ricardo Serathiuk
+VM499:2 16 VITOR OLIVEIRA DOS SANTOS
+
+document.querySelectorAll('.zWGUib').forEach(function(element, k) {
+    console.log(k + ' ' + element.innerHTML);
+});
+
+Rodando no console do browser pra pegar todos os <span> que contém os nomes dos participantes. Todos tem uma classe em comum mas o nome 'zWGUib'. Aparentemente ela não muda mas é um ponto de atenção.
+
+Daí usa o https://www.random.org/lists/ pra selecionar um felizardo pra responder.
+
+Sugestão só =D
+
+https://huggingface.co/spaces/adolfont/learn4elixir
+
+Transcribing an audio file into a .srt file
+https://gist.github.com/adolfont/6452374069e7a5cb4b9bf52febdffe00
+
+- State
+
+
+LISTEN WITH THE STUDENTS AND THEN DRAW
+
+- [Teaching Functional Programming, Software Unscripted](https://open.spotify.com/episode/0RhRifMc0XzigROlXRQPDM)
+
+- [Transcription](https://sites.google.com/noredink.com/podcast-transcriptions/1-2-teaching-functional-programming?authuser=0)
+
+  - How do you define functional programming? 
+    - art
+    - pure functions
+    - inputs, arguments, information, output
+    - no side effects
+    - nothing goes to the console
+    - no other impact in the world
+
+
+    - Functional programming style
+      - avoiding mutation and side effects
+        - pure function
+    - Functional programming languages
+
+
+- Programming withou state 
+ - Cherry notebook https://dev.to/cherryramatis/handling-state-between-multiple-instances-with-elixir-4jm1
+
+- Memory
+- Storage
+- Variables as storage address vs name for value
+- Mutation
+
+
+    MY VIDEO
+
+
+    EXERCISE: smallest largest
---
+++ examples/lean/examples_1_5.lean
@@ -0,0 +1,84 @@
+-- Learn more about lean at:
+-- https://lean-lang.org/
+-- Functional Programming in Lean, by David Thrane Christiansen https://lean-lang.org/functional_programming_in_lean/
+-- You can use Lean in your browser at https://live.lean-lang.org/
+
+-- Language version:
+-- Lean (version 4.0.0-nightly-2023-03-10, commit 0477276f66b0, Release)
+
+-- VS Code plugin version:
+-- lean4  v0.0.113
+-- Lean 4 language support for VS Code
+
+-- 1.5 Datatypes and Patterns
+-- https://lean-lang.org/functional_programming_in_lean/getting-to-know/datatypes-and-patterns.html
+
+-- "Lean constructors correspond to objects in JavaScript or TypeScript that include a tag that identifies the contents."
+
+-- Pattern Matching
+
+def isZero (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => true
+  | Nat.succ k => false
+
+
+#eval isZero Nat.zero
+#eval isZero 5
+
+def pred (n : Nat) : Nat :=
+  match n with
+  | Nat.zero => Nat.zero
+  | Nat.succ k => k
+
+#eval pred 5
+
+
+structure Point3D where
+  x : Float
+  y : Float
+  z : Float
+deriving Repr
+
+-- "In this case, it would have been much simpler to just use the z accessor, but structure patterns are occasionally the simplest way to write a function."
+def depth (p : Point3D) : Float :=
+  match p with
+  | { x:= h, y := w, z := d } => d
+
+-- Recursive Functions
+-- https://lean-lang.org/functional_programming_in_lean/getting-to-know/datatypes-and-patterns.html#recursive-functio
+
+#eval "Definitions that refer to the name being defined are called recursive definitions."
+
+-- Recursive datatypes are nicely complemented by recursive functions.
+
+#eval "structural recursion"
+def even (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => true
+  | Nat.succ k => not (even k)
+
+#eval "Unlike many languages, Lean ensures by default that every recursive function will eventually reach a base case. "
+
+-- error
+-- def evenLoops (n : Nat) : Bool :=
+--   match n with
+--   | Nat.zero => true
+--   | Nat.succ k => not (evenLoops n)
+
+def plus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k'
+
+  => Nat.succ (plus n k')
+
+def times (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => Nat.zero
+  | Nat.succ k' => plus n (times n k')
+
+def minus (n : Nat) (k : Nat) : Nat :=
+  match k with
+  | Nat.zero => n
+  | Nat.succ k' => pred (minus n k')
