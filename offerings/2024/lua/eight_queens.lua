0d413d1006b26efa94ff3aeb4e1b2cae1d69d767
Adolfo Neto <adolfo.usp@gmail.com>
2024-11-01 10:47:23 -0300

    Eight Queens

---
+++ offerings/2024/lua/eight_queens.lua
@@ -0,0 +1,45 @@
+-- Solution for the Eigth Queens Problem
+-- Adapted from Roberto Ierusalismchy's Programming in Lua, Fourth Edition https://amzn.to/3CccLTZ
+
+N = 8 -- board size
+
+-- check whether position (line, column) is free from attacks
+local function is_place_ok(board, line, column)
+    for i = 1, line - 1 do                       -- for each queen already placed
+        if (board[i] == column) or               -- same column?
+            (board[i] - i == column - line) or   -- same daigonal?
+            (board[i] + i == column + line) then -- same daigonal?
+            return false                         -- place can be attacked
+        end
+    end
+    return true -- no attacks; place is OK
+end
+
+local function print_board(board)
+    for i = 1, N do     -- for each row
+        for j = 1, N do -- and for each column
+            -- write "X" or "-" plus a space
+            io.write(board[i] == j and "X" or "-", " ")
+        end
+        io.write("\n")
+    end
+    io.write("\n")
+end
+
+
+local function add_queen(board, line) 
+    if line > N then -- all queens have been placed?
+        print_board(board) 
+    else -- try to place line-th queen
+        for column = 1, N do
+            if is_place_ok(board, line, column) then
+                board[line] = column -- place line-th queen at 'column'
+                add_queen(board, line + 1)
+            end
+        end
+    end   
+end
+
+-- run the program
+
+add_queen({}, 1)
